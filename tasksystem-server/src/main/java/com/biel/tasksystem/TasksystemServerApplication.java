80f929ad46fdd2d788a628f3c1e03077fb4b4e8e
Biel Simon Rojas <bielsimon2@gmail.com>
2016-11-11 22:15:56 +0100

    Initial commit

---
+++ tasksystem-server/src/main/java/com/biel/tasksystem/TasksystemServerApplication.java
@@ -0,0 +1,12 @@
+package com.biel.tasksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksystemServerApplication.class, args);
+	}
+}
